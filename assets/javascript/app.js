4213c8855a1e916e02c78842d16c00ee2bf721c0
Paul Nelson <pmnelso@gmail.com>
2019-03-09 06:48:01 -0500

    created HTML and began javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,79 @@
+//set initial variables
+var start = document.getElementById("#start");
+var begin = document.getElementById("#begin");
+var quizBox = document.getElementById("#quizBox");
+var question = document.getElementById("#queston");
+var innerQBox = document.getElementById("#innerQBox");
+var choices = document.getElementById("#choices");
+var A = document.getElementById("#A");
+var B = document.getElementById("#B");
+var C = document.getElementById("#C");
+var resultPage = document.getElementById("#resultPage");
+var result = document.getElementById("#result");
+var timer = document.getElementById("#timer");
+
+//create question array
+
+var questions = [
+    {question: "Question 1",
+     choiceA: correct,
+     choiceB: incorrect,
+     choiceC: incorrect,
+     rightAnswer: A
+    },
+    {question: "Question 2",
+     choiceA: correct,
+     choiceB: incorrect,
+     choiceC: incorrect,
+     rightAnswer: A
+    },
+    {question: "Question 3",
+     choiceA: correct,
+     choiceB: incorrect,
+     choiceC: incorrect,
+     rightAnswer: A
+    },
+    {question: "Question 4",
+     choiceA: correct,
+     choiceB: incorrect,
+     choiceC: incorrect,
+     rightAnswer: A
+    },
+    {question: "Question 5",
+     choiceA: correct,
+     choiceB: incorrect,
+     choiceC: incorrect,
+     rightAnswer: A
+    },
+    {question: "Question 6",
+     choiceA: correct,
+     choiceB: incorrect,
+     choiceC: incorrect,
+     rightAnswer: A
+    },
+    {question: "Question 7",
+     choiceA: correct,
+     choiceB: incorrect,
+     choiceC: incorrect,
+     rightAnswer: A
+    },
+    {question: "Question 8",
+     choiceA: correct,
+     choiceB: incorrect,
+     choiceC: incorrect,
+     rightAnswer: A
+    },
+    {question: "Question 9",
+     choiceA: correct,
+     choiceB: incorrect,
+     choiceC: incorrect,
+     rightAnswer: A
+    },
+    {question: "Question 10",
+     choiceA: correct,
+     choiceB: incorrect,
+     choiceC: incorrect,
+     rightAnswer: A
+    },
+];
+
